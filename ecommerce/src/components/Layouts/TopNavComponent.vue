cf9f080a5826830513100bf623362845686c431c
AayushN01 <aayushniraula1@gmail.com>
2024-06-08 09:35:52 +0545

    topnavcomponent

--- ecommerce/src/components/Layouts/TopNavComponent.vue
+++ ecommerce/src/components/Layouts/TopNavComponent.vue
@@ -1,4 +1,5 @@
 <script>
+
     export default {
         name: 'TopNavComponent'
     }
@@ -7,8 +8,18 @@
 <template>
     <div class="topNavBar">
         <div class="social-icons">
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Youtube</a></li>
+                <li><a href="#">Twitter</a></li>
+            </ul>
         </div>
         <div class="user-login">
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
         </div>
     </div>
 </template>
