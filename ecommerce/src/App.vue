d2f6c91955ac4b3da98a296ef05021c4cb016d69
AayushN01 <aayushniraula1@gmail.com>
2024-06-08 13:26:58 +0545

    Product Section

--- ecommerce/src/App.vue
+++ ecommerce/src/App.vue
@@ -2,13 +2,15 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HeaderComponent from './components/Layouts/HeaderComponent.vue'
 import TopNavComponent from './components/Layouts/TopNavComponent.vue'
-import HeroComponent from './components/HeroComponent.vue';
+import HeroComponent from './components/HeroComponent.vue'
+import ProductComponent from './components/ProductComponent.vue'
 
 export default {
   components:{
     HeaderComponent,
     TopNavComponent,
-    HeroComponent
+    HeroComponent,
+    ProductComponent
   }
 
 }
@@ -19,5 +21,6 @@ export default {
   <TopNavComponent></TopNavComponent>
   <HeaderComponent />
   <HeroComponent />
+  <ProductComponent/>
  <RouterView />
 </template>
---
+++ ecommerce/src/components/ProductComponent.vue
@@ -0,0 +1,45 @@
+<script>
+    import productImage from '@/assets/images/product.png';
+
+    export default {
+        name: 'ProductComponent',
+        data(){
+            return{
+                productImg: productImage
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="newArrivalsSection section-padding">
+        <div class="section-title section-title-padding">
+            <h2 class="text-uppercase text-center">New Arrivals</h2>
+        </div>
+        <div class="products-section container">
+            <div class="product-list">
+                <div class="product-item" v-for="item in 4" :key="item">
+                    <div class="product-item-img">
+                        <img :src="productImg" alt="Product">
+                    </div>
+                    <div class="product-item-content">
+                        <div class="productName">
+                            <h6>Hiking Shoe</h6>
+                        </div>
+                        <div class="productPrice">
+                            <p>$500</p>
+                        </div>
+                        <div class="product-item-btns">
+                            <a class="btn btn-primary">Add To Cart</a>
+                            <a class="btn btn-primary">View Product</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="sectionBtn">
+                <button class="btn btn-primary">View All Products</button>
+            </div>
+        </div>
+    </div>
+</template>
