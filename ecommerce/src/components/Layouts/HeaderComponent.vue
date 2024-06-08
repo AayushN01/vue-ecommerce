084f261b70c120f299028695260034e4a2b07a99
AayushN01 <aayushniraula1@gmail.com>
2024-06-08 12:13:50 +0545

    Navbar Component

--- ecommerce/src/components/Layouts/HeaderComponent.vue
+++ ecommerce/src/components/Layouts/HeaderComponent.vue
@@ -1,9 +1,43 @@
 <script>
+import { RouterLink, RouterView } from 'vue-router'
+
+import logoImage from '@/assets/images/logo-new.png';
 export default {
     name:'HeaderComponent',
+    data(){
+        return {
+            logo: logoImage
+        };
+    }
 }
 </script>
 
 <template>
-    <h1>Header Component</h1>
+    <nav>
+        <div class="navbar-logo">
+            <img :src="logo" alt="Logo">
+        </div>
+        <div class="navbar-menu">
+            <ul class="menuList">
+                <li class="menu-item">
+                    <router-link to="/">Home</router-link>
+                </li>
+                <li class="menu-item">
+                    <router-link to="/">About</router-link>
+                </li>
+                <li class="menu-item">
+                    <router-link to="/">Shop</router-link>
+                </li>
+                <li class="menu-item">
+                    <router-link to="/">Blogs</router-link>
+                </li>
+                <li class="menu-item">
+                    <router-link to="/">Contact</router-link>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-cart">
+            <a href="#">Cart <span>2</span></a>
+        </div>
+    </nav>
 </template>
